1aac06033f7e0c1719e97b2db9af7316a502b7ab
itizidon <dng21@binghamton.edu>
2020-08-06 12:20:28 -0400

    fixed db stuff

--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
+const pkg = require('../package.json')
+const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
+
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(`postgres://localhost:5432/stackadon`)
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: false
+  }
+)
 
 const User = sequelize.define(
   'user',
